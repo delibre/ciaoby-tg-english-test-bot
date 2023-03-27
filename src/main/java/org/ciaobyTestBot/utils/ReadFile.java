767b9f9c5be32e656508a54065da15baaea0f227
delibre <viacheslav.bylo@gmail.com>
2023-03-27 20:00:38 +0200

    fixed bug with jar not reading questions30.ods file

--- src/main/java/org/ciaobyTestBot/utils/ReadFile.java
+++ src/main/java/org/ciaobyTestBot/utils/ReadFile.java
@@ -3,23 +3,25 @@ package org.ciaobyTestBot.utils;
 import com.aspose.cells.Workbook;
 import org.ciaobyTestBot.englishtest.Question;
 
+import java.io.InputStream;
 import java.util.LinkedList;
 
 public class ReadFile {
 
     public static LinkedList<Question> readFile() throws Exception {
-        var book = new Workbook( "src/main/resources/questions30.ods");
+        InputStream inputStream = ReadFile.class.getResourceAsStream("/questions30.ods");
+        var book = new Workbook(inputStream);
         var sheet = book.getWorksheets().get(0);
         var cells = sheet.getCells();
         LinkedList<Question> questions = new LinkedList<>();
 
         for (int row = 0; row < cells.getMaxDataRow() + 1; row++) {
             questions.add(new Question(
-                         row+1,
-                                        cells.get(row, 1).getStringValue(),
-                                        cells.get(row, 2).getStringValue().split("\\|"),
-                                        cells.get(row, 3).getIntValue()
-                                        ));
+                    row+1,
+                    cells.get(row, 1).getStringValue(),
+                    cells.get(row, 2).getStringValue().split("\\|"),
+                    cells.get(row, 3).getIntValue()
+            ));
         }
 
         return questions;

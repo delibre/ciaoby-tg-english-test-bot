0ed45636205678e2ec9df70f2c1118796919299f
delibre <viacheslav.bylo@gmail.com>
2023-03-27 21:14:42 +0200

    fixed regex bug

--- src/main/java/org/ciaobyTestBot/utils/Regex.java
+++ src/main/java/org/ciaobyTestBot/utils/Regex.java
@@ -5,9 +5,9 @@ import java.util.regex.Pattern;
 public class Regex {
     public static boolean checkPhoneNumber(String phoneNumber) {
         String patterns
-                = "^(\\+\\d{1,3}( )?)?((\\(\\d{3}\\))|\\d{3})[- .]?\\d{3}[- .]?\\d{4}$"
-                + "|^(\\+\\d{1,3}( )?)?(\\d{3}[ ]?){2}\\d{3}$"
-                + "|^(\\+\\d{1,3}( )?)?(\\d{3}[ ]?)(\\d{2}[ ]?){2}\\d{2}$";
+                = "^(\\+?\\d{1,3}( )?)?((\\(\\d{3}\\))|\\d{3})[- .]?\\d{3}[- .]?\\d{4}$"
+                + "|^(\\+?\\d{1,3}( )?)?(\\d{3}''?){2}\\d{3}$"
+                + "|^(\\+?\\d{1,3}( )?)?(\\d{3}''?)(\\d{2}''?){2}\\d{2}$";
 
 
         return Pattern.compile(patterns).matcher(phoneNumber).matches();

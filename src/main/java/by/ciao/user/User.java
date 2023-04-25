d54a2389f0f0f87b58da54f198515da855a0616a
delibre <viacheslav.bylo@gmail.com>
2023-04-25 21:10:30 +0200

    refactored User class

--- src/main/java/by/ciao/user/User.java
+++ src/main/java/by/ciao/user/User.java
@@ -27,14 +27,6 @@ public class User {
         this.testState = new UsersTestState();
     }
 
-    public User(Long chatId, String fullName, String username, String phone, String referral) {
-        this.chatId = chatId;
-        this.fullName = fullName;
-        this.username = username;
-        this.phone = phone;
-        this.referral = referral;
-    }
-
     public void clearTest() throws Exception {
         testState = new UsersTestState();
         lastMessage = null;
